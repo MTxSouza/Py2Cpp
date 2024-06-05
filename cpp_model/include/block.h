f43f650b7634189d3e2cbaa9e91b3658e3a3e68c
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-06-04 22:34:19 -0300

    fix: Initialize layers with a different way in builder.

--- cpp_model/include/block.h
+++ cpp_model/include/block.h
@@ -12,25 +12,17 @@ class ConvBlockImpl : public nn::Module {
     public: // Definitions.
 
         nn::Sequential layer{nullptr};
-        nn::Conv2d conv{nullptr};
-        nn::ReLU relu{nullptr};
-        nn::Dropout dp{nullptr};
-        nn::MaxPool2d pool{nullptr};
 
     public: // Methods.
 
         // Builder.
-        ConvBlockImpl(int in_channels, int out_channels, float dropout) {
-
-            // Layers.
-            layer = register_module("layer", nn::Sequential(
+        ConvBlockImpl(int in_channels, int out_channels, float dropout)
+            : layer(register_module("layer", nn::Sequential(
                 nn::Conv2d(nn::Conv2dOptions(in_channels, out_channels, 5).padding(2)),
                 nn::ReLU(),
                 nn::Dropout(nn::DropoutOptions(dropout)),
                 nn::MaxPool2d(nn::MaxPool2dOptions({2, 2}).stride(2))
-            ));
-
-        }
+            ))) {}
 
         // Forward Pass.
         torch::Tensor forward(torch::Tensor x) {
@@ -48,21 +40,14 @@ class ClassifierImpl : public nn::Module {
     
         ConvBlock conv1{nullptr}, conv2{nullptr};
         nn::Linear fc{nullptr};
-        int imgFeatures;
 
     public:
 
         // Builder.
-        ClassifierImpl(int img_size, int image_channels, int feature_scale, int n_classes, float dropout) {
-                
-                // Convolutional Blocks.
-                conv1 = register_module("conv1", ConvBlock(image_channels, feature_scale, dropout));
-                conv2 = register_module("conv2", ConvBlock(feature_scale, feature_scale * 2, dropout));
-    
-                // Fully Connected Layer.
-                int imgFeatures = ((img_size / 4) * (img_size / 4)) * feature_scale * 2;
-                fc = register_module("fc", nn::Linear(imgFeatures, n_classes));
-        }
+        ClassifierImpl(int img_size, int image_channels, int feature_scale, int n_classes, float dropout)
+            : conv1(register_module("conv1", ConvBlock(image_channels, feature_scale, dropout))),
+              conv2(register_module("conv2", ConvBlock(feature_scale, feature_scale * 2, dropout))),
+              fc(register_module("fc", nn::Linear(((img_size / 4) * (img_size / 4)) * feature_scale * 2, n_classes))) {}
 
         // Forward method.
         torch::Tensor forward(torch::Tensor x) {
